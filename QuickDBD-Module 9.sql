4a3580d51617421085b65a8602cc7f3dc94309f8
ShamiahL <136922829+ShamiahL@users.noreply.github.com>
2023-08-30 22:16:55 -0400

    Add files via upload

---
+++ QuickDBD-Module 9.sql
@@ -0,0 +1,49 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/OEsjeu
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "EMPLOYEES" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(40)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(35)   NOT NULL,
+    "last_name" VARCHAR(35)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_EMPLOYEES" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "DEPARTMENTS" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_DEPARTMENTS" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "DEPT_EMP" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL
+);
+
+CREATE TABLE "DEPT_MANAGER" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "SALARIES" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "TITLES" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(40)   NOT NULL,
+    CONSTRAINT "pk_TITLES" PRIMARY KEY (
+        "title_id"
+     )
+);
+
